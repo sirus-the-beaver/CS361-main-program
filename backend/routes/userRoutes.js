e1f4afcb4cfbc8e58799f6017af1afac5e772813
Sirus Salari <salaris@oregonstate.edu>
2024-10-31 03:11:10 -0700

    Add route for preferences

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -38,4 +38,20 @@ router.post("/register", async(req, res) => {
     }
 });
 
+router.put("/preferences", async(req, res) => {
+    const { id } = req.user;
+    const { preferences } = req.body;
+
+    try {
+        const user = await User.findById(id);
+        user.preferences = preferences;
+        await user.save();
+
+        res.status(200).json({ message: "Preferences updated" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+});
+
 module.exports = router;
