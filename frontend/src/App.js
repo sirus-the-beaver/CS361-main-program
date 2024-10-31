492809f953df179147d65d579303c57fba0b9c14
Sirus Salari <salaris@oregonstate.edu>
2024-10-31 04:00:41 -0700

    Define routes for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import { useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Signup from './components/Signup';
+import Preferences from './components/Preferences';
 
 function App() {
   const navigate = useNavigate();
@@ -8,6 +10,10 @@ function App() {
       <header>
         <button onClick={() => navigate("/signup")} className='p-2 bg-blue-500 text-white rounded'>Sign Up</button>
       </header>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/preferences" element={<Preferences />} />
+      </Routes>
     </div>
   );
 }
