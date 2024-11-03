0de600172bb9451cb0c8e0c4b72b237e8616842e
Sirus Salari <salaris@oregonstate.edu>
2024-11-02 22:59:10 -0700

    Call Spoonacular API in backend to return recipes

---
+++ backend/routes/recipeRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+
+router.post('/recipes', (req, res) => {
+    const { ingredients } = req.body;
+    
+    const recipes = async (ingredients) => {
+        try {
+            const response = await axios.post('https://api.spoonacular.com/recipes/findByIngredients', {
+                apiKey: process.env.SPOONACULAR_API_KEY,
+                ingredients: ingredients.join(','),
+                ranking: 2
+            });
+            return response;
+        } catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+    res.json(recipes(ingredients));
+})
