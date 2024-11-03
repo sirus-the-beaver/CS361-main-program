99587f5e129768f3d0fcbd1afe625ec7f4c6aa1b
Sirus Salari <salaris@oregonstate.edu>
2024-11-02 23:16:31 -0700

    Integrate the sign in component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Signup from './components/Signup';
+import SignIn from './components/SignIn';
 import Preferences from './components/Preferences';
 import IngredientInput from './components/IngredientInput';
 
@@ -10,9 +11,11 @@ function App() {
     <div>
       <header>
         <button onClick={() => navigate("/signup")} className='p-2 bg-blue-500 text-white rounded'>Sign Up</button>
+        <button onClick={() => navigate("/signin")} className='p-2 bg-blue-500 text-white rounded'>Sign In</button>
       </header>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<SignIn />} />
         <Route path="/preferences" element={<Preferences />} />
         <Route path="/ingredient-input" element={<IngredientInput />} />
       </Routes>
