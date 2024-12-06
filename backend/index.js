f55ca6e2fd68e50be333341b3694b70761477274
Sirus Salari <salaris@oregonstate.edu>
2024-12-05 23:10:26 -0800

    Send all non-api requests to react router

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
