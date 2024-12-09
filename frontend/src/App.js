2635ea0cc6e443b530027eea6d59fb95af9f5e51
Sirus Salari <salaris@oregonstate.edu>
2024-12-09 13:18:40 -0800

    Add hamburger menu for smaller screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from 'react';
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
+import { AiOutlineMenu, AiOutlineCloseCircle } from 'react-icons/ai';
 import { AuthContext } from './context/AuthContext';
 import Signup from './components/Signup';
 import SignIn from './components/SignIn';
@@ -11,12 +12,14 @@ import RecipeDetail from './components/RecipeDetail';
 import RecipeRecommendations from './components/RecipeRecommendations';
 import WineRecommendation from './components/WineRecommendation';
 import DishRecommendation from './components/DishRecommendation';
+import { set } from 'mongoose';
 
 function App() {
   const navigate = useNavigate();
   const { auth, login } = useContext(AuthContext);
   const isAuthenticated = !!auth.token;
   const [authLoading, setAuthLoading] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);  
 
   useEffect(() => {
     const token = localStorage.getItem('token');
@@ -43,18 +46,38 @@ function App() {
     return children;
   }
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
     <div>
       <header className="bg-gray-800 py-4">
         <div className="container mx-auto flex flex-wrap justify-between items-center px-4">
           <h1 className="text-white text-2xl font-bold">DishFindr</h1>
+          <button className="text-white text-2xl sm:hidden focus:outline-none" onClick={toggleMenu}>
+            {menuOpen ? <AiOutlineCloseCircle /> : <AiOutlineMenu />}
+          </button>
           {auth.token ? (
-            <nav className="w-full sm:w-auto">
+            <nav className={`w-full sm:w-auto sm:flex ${menuOpen ? "block" : "hidden"} bg-gray-700 sm:bg-transparent sm:static absolute top-full left-0`}>
               <SignOut />
-              <button className="text-white text-base sm:text-lg ml-0 sm:ml-4 mt-2 sm:mt-0 font-medium" onClick={() => navigate('/preferences')}>Preferences</button>
-              <button className="text-white text-base sm:text-lg ml-0 sm:ml-4 mt-2 sm:mt-0 font-medium" onClick={() => navigate('/ingredient-input')}>Ingredient Input</button>
-              <button className="text-white text-base sm:text-lg ml-0 sm:ml-4 mt-2 sm:mt-0 font-medium" onClick={() => navigate('/wine-recommendation')}>Wine Recommendations</button>
-              <button className="text-white text-base sm:text-lg ml-0 sm:ml-4 mt-2 sm:mt-0 font-medium" onClick={() => navigate('/dish-recommendation')}>Dish Recommendations Based on Wine</button>
+              <button className="block sm:inline-block text-white text-base sm:text-lg p-4 sm:p-0 sm:ml-4 font-medium" onClick={() => {
+                setMenuOpen(false);
+                navigate('/preferences');
+                setMenuOpen(false);
+              }}>Preferences</button>
+              <button className="block sm:inline-block text-white text-base sm:text-lg p-4 sm:p-0 sm:ml-4 font-medium" onClick={() => {
+                setMenuOpen(false);
+                navigate('/ingredient-input');
+              }}>Ingredient Input</button>
+              <button className="block sm:inline-block text-white text-base sm:text-lg p-4 sm:p-0 sm:ml-4 font-medium" onClick={() => {
+                setMenuOpen(false);
+                navigate('/wine-recommendation');
+              }}>Wine Recommendations</button>
+              <button className="block sm:inline-block text-white text-base sm:text-lg p-4 sm:p-0 sm:ml-4 font-medium" onClick={() => {
+                setMenuOpen(false);
+                navigate('/dish-recommendation');
+              }}>Dish Recommendations Based On Wine</button>
             </nav>
           ) : (
             <nav className="w-full sm:w-auto">
